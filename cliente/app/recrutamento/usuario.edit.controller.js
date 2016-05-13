bddafcc0d474547094dd1cd9033b35535e6c7816
Gustavo Michels <ghmichels@gmail.com>
2016-05-13 16:59:30 -0300

    Melhorias Cliente Curriculo

--- cliente/app/recrutamento/usuario.edit.controller.js
+++ cliente/app/recrutamento/usuario.edit.controller.js
@@ -1,13 +1,16 @@
 (function () {
     'use strict';
-    angular.module('app').controller('UsuarioEditController', ['$mdToast', '$http', 'Usuario', '$state', '$stateParams', 'Workspace', UsuarioEditController]);
+    angular.module('app').controller('UsuarioEditController', ['$mdToast', '$http', 'Usuario', '$state', '$stateParams', 'Workspace', 'Cidade', 'Escolaridade', UsuarioEditController]);
 
     var state = "usuario"
-    function UsuarioEditController($mdToast, $http, Usuario, $state, $stateParams, Workspace) {
+    function UsuarioEditController($mdToast, $http, Usuario, $state, $stateParams, Workspace, Cidade, Escolaridade) {
         var vm = this;
         vm.entity = {}
         vm.sexos = []
-        
+        vm.escolaridades = []
+        vm.senhaConf
+        vm.querySearchCidade = querySearchCidade
+
         Workspace.title = "Manutenção de Usuário";
         if ($stateParams.id) {
             Workspace.loading("Carregando...", Usuario.get({id: $stateParams.id}).$promise.then(function (data) {
@@ -20,6 +23,7 @@
             vm.entity = new Usuario()
 
         loadSexos()
+        loadEscolaridades()
         vm.save = save;
         vm.cancel = cancel;
         function save($event, $valid) {
@@ -43,6 +47,19 @@
         }
         function callbackError() {
             Workspace.showMessage("Ocorreu um erro ao salvar o registro")
+        }
+        
+        function loadEscolaridades() {
+            Escolaridade.query().$promise.then(function (resposta) {
+                vm.escolaridades = resposta;
+            })
+        }
+
+        function querySearchCidade(texto) {
+            return Cidade.query({
+                filter: texto,
+                limit: 10
+            }).$promise
         }
 
     }
