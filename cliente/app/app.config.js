562785e17584dc09cff0f1e03caf6a6e6cfa36ba
Jn0066743 <micheljeni2306@gmail.com>
2016-05-30 21:08:46 -0300

    Cuidando do Layout

--- cliente/app/app.config.js
+++ cliente/app/app.config.js
@@ -1,22 +1,23 @@
 (function () {
     angular.module('app').config(["$mdThemingProvider", '$httpProvider', '$mdIconProvider', config]);
     function config($mdThemingProvider, $httpProvider, $mdIconProvider) {
-        var customBlueMap = $mdThemingProvider.extendPalette('light-blue', {
-            'contrastDefaultColor': 'light',
-            'contrastDarkColors': ['50'],
-            '50': 'ffffff'
+        var customBlueMap = $mdThemingProvider.extendPalette('blue-grey', {
+            'contrastDefaultColor': 'blue',
+            'contrastDarkColors': ['100'],
+            '100': 'ffffff'
         });
         $mdThemingProvider.definePalette('customBlue', customBlueMap);
         $mdThemingProvider.theme('default')
-                .primaryPalette('customBlue', {
-                    'default': '500',
-                    'hue-1': '50'
+                .primaryPalette('blue-grey', {
+                    'default': '900', 
+                    'hue-1': '500',
+                    'hue-2': '900'
                 })
-                .accentPalette('pink')
+                .accentPalette('blue-grey')
 //                .backgroundPalette('grey')
 
         $mdThemingProvider.theme('input', 'default')
-                .primaryPalette('grey');
+                .primaryPalette('blue-grey');
 
         $httpProvider.interceptors.push('ApiInterceptor');
 
@@ -32,17 +33,27 @@
                 .icon('message', 'img/icons/message.svg', 24)
                 .icon('copy2', 'img/icons/copy2.svg', 24)
                 .icon('facebook', 'img/icons/facebook.svg', 24)
-                .icon('twitter', 'img/icons/twitter.svg', 24)
-        
+                .icon('twitter', 'img/icons/twitter.svg', 24)                                  
+                
+                .icon('tabela', 'assets/icon/spreadsheet.svg', 48)    
                 .icon('eventos', 'assets/icon/signing-receipt.svg', 48)
-                .icon('folha', 'assets/icon/bill.svg', 48)
-                .icon('tabela', 'assets/icon/spreadsheet.svg', 48)        
                 .icon('calculator', 'assets/icon/calculator.svg', 48)
+                .icon('folha', 'assets/icon/file-black-text-paper-sheet-symbol-with-one-folded-corner.svg', 48)      
         
+                .icon('ficha', 'assets/icon/ficha-funcional.svg', 48)
+                .icon('cargos', 'assets/icon/stationery-product.svg', 48)
+                .icon('cbo', 'assets/icon/bar-code.svg', 48)
+                .icon('qualificacao', 'assets/icon/3-star-hotel.svg', 48)
                 .icon('acidente', 'assets/icon/car-first-aid-kit.svg', 48)
                 .icon('sindicato', 'assets/icon/workers-team.svg', 48)
+                .icon('empresa', 'assets/icon/buildings.svg', 48)
+                .icon('unidade', 'assets/icon/buildings-unidades.svg', 48)
                 .icon('curriculo', 'assets/icon/curriculos.svg', 48)
-                .icon('qualificacao', 'assets/icon/3-star-hotel.svg', 48)
+        
+                .icon('necessidade', 'assets/icon/business-man.svg', 48)
+                .icon('vagas', 'assets/icon/jobs-open.svg', 48)
+                .icon('avaliacao', 'assets/icon/favorites-list.svg', 48)
+                
                 .icon('faixa', 'assets/icon/stock-earnings.svg', 48)
                 .icon('user', 'assets/icon/users.svg', 48);
                 
