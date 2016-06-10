bc098bdd6b67cbfa1eff314a59e9c1739fdb56e3
Jn0066743 <micheljeni2306@gmail.com>
2016-06-09 22:08:26 -0300

    Cuidando do Layout

--- cliente/app/app.constants.js
+++ cliente/app/app.constants.js
@@ -2,10 +2,9 @@
     angular.module('app').constant("config", {
         appName: "App",
         appVersion: 2.0,
-            imageUrl:"http://localhost:8384/rh/images", 
-            apiUrl: "http://localhost:8384/rh/api",
-            hubUrl: "ws://localhost:8384/rh/hub"
-            
+            imageUrl:"http://localhost:8390/rh/images", 
+            apiUrl: "http://localhost:8390/rh/api",
+            hubUrl: "ws://localhost:8390/rh/hub"            
     });
 })();
 
